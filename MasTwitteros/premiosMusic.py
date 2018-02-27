a3042f1e42dca3e628e5ed8a899dbbcc829c4e5a
Stalin <stalinct97@gmail.com>
2018-02-26 23:21:09 -0500

    New Versión
    Adding the Database address

---
+++ MasTwitteros/premiosMusic.py
@@ -0,0 +1,102 @@
+import couchdb #Libreria de CouchDB (requiere ser instalada primero)
+from tweepy import Stream
+#tweepy es la libreria que trae tweets desde
+#la API de Twitter (requiere ser instalada primero)
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json #Libreria para manejar archivos JSON
+import time #tiempo
+
+###Credenciales de la cuenta de Twitter########################
+#Poner aqui las credenciales de su cuenta privada, caso contrario la API
+#bloqueara esta cuenta de ejemplo
+ckey = "YOUR CKEY"
+csecret = "YOUR CSECRET"
+atoken = "YOUR ATOKEN"
+asecret = "YOUR ASECRET"
+#####################################
+#VARIABLES
+
+QUITO_LOC = [-78.710331,-0.446671,-78.241671,0.036792]
+SECTOR_QUITO = [-78.5236433689,-0.2528339981,-78.5218677466,-0.2524263063]
+TENDENCIA = ['#BRITVIDANNEMARIE','#BRITVIDEDSHEERAN','#BRITs2018','#BRITs',
+'#BritAwards','#BRITAwards','@AnneMarieIAm','@edsheeran',
+'#Brits','#Brits','@BRITs','@BRITs']
+
+#####################################
+
+class listener(StreamListener):
+
+    def __init__(self, path=None):
+        self.path = path
+        self.siesta = 0
+        self.nightnight = 0
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            dictTweet["_id"] = str(dictTweet['id'])
+            #Antes de guardar el documento puedes realizar parseo, limpieza y
+            #cierto analisis o filtrado de datos previo
+            #a guardar en documento en la base de datos
+            doc = db.save(dictTweet) #Aqui se guarda el tweet en la base de couchDB
+            print("Guardado " + "=> " + dictTweet["_id"])
+        except:
+            print("Documento ya existe")
+            pass
+        return True
+    
+    def on_error(self, status):
+        print ('Error:', str(status_code))
+
+        if status_code == 420:
+            sleepy = 60 * math.pow(2, self.siesta)
+            print(time.strftime("%Y%m%d_%H%M%S"))
+            print("A reconnection attempt will occur in " + \
+            str(sleepy/60) + " minutes.")
+            print('''
+            *******************************************************************
+            From Twitter Streaming API Documentation
+            420: Rate Limited
+            The client has connected too frequently. For example, an 
+            endpoint returns this status if:
+            - A client makes too many login attempts in a short period 
+              of time.
+            - Too many copies of an application attempt to authenticate 
+              with the same credentials.
+            *******************************************************************
+            ''')
+            time.sleep(sleepy)
+            self.siesta += 1
+        else:
+            sleepy = 5 * math.pow(2, self.nightnight)
+            print(time.strftime("%Y%m%d_%H%M%S"))
+            print("A reconnection attempt will occur in " + \
+            str(sleepy) + " seconds.")
+            time.sleep(sleepy)
+            self.nightnight += 1
+        return True   
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+
+#Setear la URL del servidor de couchDB
+server = couchdb.Server('http://localhost:5984/')
+try:
+    #Si no existe la Base de datos la crea
+    db = server.create('premiosmusic')
+except:
+    #Caso contrario solo conectarse a la base existente
+    db = server['premiosmusic']
+
+   
+    
+#Aqui se define el bounding box con los limites geograficos donde recolectar los tweets
+#twitterStream.filter(track=["quito","ecuador"])
+#twitterStream.filter(locations=QUITO_LOC)
+twitterStream.filter(track=TENDENCIA)
+#twitterStream.filter(languages=['es'],track=["quito","ecuador"], async=True)
+
+##
